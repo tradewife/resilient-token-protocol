28fe626ca9164450c142f69d9a03f46c5a23daea
tradewife <katejcooper.atelier@gmail.com>
2026-08-04 16:34:11 +1000

    feat(research): register S14 Marubozu Retracement in PLUGINS registry

--- research/strategy_plugins/__init__.py
+++ research/strategy_plugins/__init__.py
@@ -5,6 +5,7 @@ from research.strategy_plugins.s04_rsi_exhaustion import RSIExhaustionPlugin
 from research.strategy_plugins.s06_vol_squeeze import VolSqueezePlugin
 from research.strategy_plugins.s13_adx_trend import ADXTrendPlugin
 from research.strategy_plugins.s10_momentum_divergence import MomentumDivergencePlugin
+from research.strategy_plugins.s14_marubozu_retracement import MarubozuRetracementPlugin
 
 PLUGINS = {
     "S02": BreakoutBandPlugin,
@@ -12,4 +13,5 @@ PLUGINS = {
     "S06": VolSqueezePlugin,
     "S10": MomentumDivergencePlugin,
     "S13": ADXTrendPlugin,
+    "S14": MarubozuRetracementPlugin,
 }
